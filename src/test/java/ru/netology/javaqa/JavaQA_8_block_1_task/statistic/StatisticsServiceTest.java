bcbbda86a1571cfeddc8203957d0f0a1294c6a01
Alexey <avkir08@ya.ru>
2025-08-16 01:01:55 +0400

    full test coverage

--- src/test/java/ru/netology/javaqa/JavaQA_8_block_1_task/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/javaqa/JavaQA_8_block_1_task/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxWhenMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 15, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaxLast() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 20};
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
